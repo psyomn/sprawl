4a9ea9df7e86de78ad5efd3a33ba71bd0d1b73ae
Simon Symeonidis <lethaljellybean@gmail.com>
2021-02-26 00:21:53 -0500

    [misc] Add find_if, count_if on object

--- misc/algorithm.cc
+++ misc/algorithm.cc
@@ -7,12 +7,33 @@
 #include "gmock/gmock.h"
 
 #include <algorithm>
+#include <cstdint>
 #include <string>
 #include <vector>
+
+/** test objects */
+
+class Person {
+public:
+  Person(std::string name, std::uint8_t age) :
+    name_(name), age_(age) {}
+  const std::string& GetName() const noexcept { return name_; }
+  std::uint8_t GetAge() const noexcept { return age_; }
+private:
+  std::string name_;
+  std::uint8_t age_;
+};
 
 static const std::vector<std::string> kStrVec = {
   "one", "two", "three", "four", "potato"
 };
+
+static const std::vector<Person> kPeople = {
+  Person("bob", 12), Person("jane", 13), Person("chris", 14),
+  Person("barbara", 12), Person("nope", 12),
+};
+
+/** tests */
 
 TEST(algorithm, vector_all_of) {
   {
@@ -41,5 +62,21 @@ TEST(algorithm, vector_find_if) {
                    });
 
     EXPECT_TRUE(it == kStrVec.cend());
+  }
+}
+
+TEST(algorithm, vector_find_if_object) {
+  {
+    std::vector<Person>::const_iterator it =
+      std::find_if(kPeople.cbegin(), kPeople.cend(),
+                   [](const Person& p) { return p.GetName() == "jane"; });
+    EXPECT_TRUE(it != kPeople.cend());
+  }
+
+  {
+    const ssize_t count =
+      std::count_if(kPeople.cbegin(), kPeople.cend(),
+                   [](const Person& p) { return p.GetAge() % 2 == 0; });
+    EXPECT_EQ(count, 4);
   }
 }
