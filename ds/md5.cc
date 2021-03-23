434c7f3531f7856be3172f8c708bfee366c0dc57
Simon Symeonidis <lethaljellybean@gmail.com>
2021-03-22 22:48:02 -0400

    [md5] Add implmentation for md5

--- ds/md5.cc
+++ ds/md5.cc
@@ -11,35 +11,74 @@ namespace psy::ds {
     6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21,
   };
 
+  constexpr size_t kSineSize = 64;
+  const std::uint32_t kSine[kSineSize] = {
+    0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+    0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+    0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+    0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+    0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+    0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+    0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+    0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+    0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+    0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+    0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+    0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+    0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+    0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+    0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+    0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391,
+  };
+
   const std::uint32_t kA0 = 0x67452301;
   const std::uint32_t kB0 = 0xefcdab89;
   const std::uint32_t kC0 = 0x98badcfe;
   const std::uint32_t kD0 = 0x10325476;
 
   std::array<std::uint32_t, 4> MD5::Digest(const std::vector<std::uint8_t>& message) {
-    auto padded_message = PadMessage(message);
+    const auto padded_message = PadMessage(message);
     const size_t padded_size = padded_message.size();
 
     std::uint32_t A0 = kA0, B0 = kB0, C0 = kC0, D0 = kD0;
-    std::uint64_t F = 0, G = 0;
 
     for (size_t i = 0; i < padded_size; i += 64) {
       std::uint32_t a = A0, b = B0, c = C0, d = D0;
 
       for (size_t j = 0; j < kShiftSize; ++j) {
+        std::uint64_t F = 0, G = 0;
+
         if (j >= 48 && j <= 63) {
           // F := C xor (B or (not D))
           // g := (7×i) mod 16
+          F = c ^ (b | (~d));
+          G = (7 * i) % 16;
         } else if (j >= 32 && j <= 47) {
           // F := B xor C xor D
           // g := (3×i + 5) mod 16
+          F = b ^ c ^ d;
+          G = (3 * i + 5) % 16;
         } else if (j >= 16 && j <= 31) {
           // F := (D and B) or ((not D) and C)
           // g := (5×i + 1) mod 16
-        } else if (j >= 0  && j <= 15) {
+          F = (d & b) | ((!d) & c);
+          G = (5 * i + 1) % 16;
+        } else if (j > 0  && j <= 15) {
           // F := (B and C) or ((not B) and D)
           // g := i
+          F = (b & c) | ((!b) & d);
+          G = i;
         }
+
+        // M[g] must be a 32-bits block
+        F = F + a + kSine[j] + padded_message[G];
+        a = d;
+        d = c;
+        c = b;
+
+        // B := B + leftrotate(F, s[i])
+        const std::uint8_t shift = kShift[j];
+        b = b + ((F << shift) | (F >> (32 - shift)));
       }
 
       A0 += a;
@@ -48,8 +87,7 @@ namespace psy::ds {
       D0 += d;
     }
 
-    std::array<std::uint32_t, 4> ret = {0, 0, 0, 0};
-    return ret;
+    return std::array<std::uint32_t, 4>{A0, B0, C0, D0};
   }
 
   std::vector<std::uint8_t> MD5::PadMessage(const std::vector<std::uint8_t>& message) {
