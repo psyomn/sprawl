0fb61e853a8e2693e8dabedd00fa6e460542fd99
Simon Symeonidis <lethaljellybean@gmail.com>
2021-04-16 01:08:01 -0400

    [misc] Rewrite multithreaded vector example
    Added some utils that I'll probably factor out

--- misc/multi-vector.cc
+++ misc/multi-vector.cc
@@ -1,9 +1,15 @@
 // TODO: redo with fork example
+//
+// Basically, wanted to dmake a very simple example, remind myself of
+// what tends to be annoying when doing multicore programming, and
+// what patterns can benefit us a little more.
+//
 #include <cstdint>
 #include <iostream>
 #include <sstream>
 #include <mutex>
 #include <thread>
+#include <utility> // pair, make_pair
 #include <vector>
 
 // C - getopt
@@ -33,8 +39,59 @@ private:
   std::mutex mut_;
 };
 
-namespace Granulator {
-}
+/**
+ * a glorified calculator
+ *
+ * should take a number of elements and suggest best partitioning
+ * between threads
+ *
+ * TODO: this might be useful enough to factor out on some common
+ *   module
+ */
+class Granulator {
+public:
+  explicit Granulator(size_t num_elements) :
+    threshold_(128),
+    num_elements_(num_elements),
+    num_threads_(std::thread::hardware_concurrency()),
+    grain_(num_elements_ / num_threads_),
+    step_(0)
+  {
+    if (num_elements_ <= num_threads_ * threshold_) {
+      // to trigger a sane work sharing environment, we'll make sure
+      // that each thread has at least "threshold_" elements to deal
+      // with
+      num_threads_ = 1;
+      grain_ = num_elements_;
+    }
+  }
+
+  inline size_t GetNumThreads() const noexcept { return num_threads_; }
+  inline size_t GetGrain() const noexcept { return grain_; }
+  inline void ResetSteps(void) { step_ = 0; }
+
+  // this returns _inclusive_ ranges, so your loops must be from
+  std::pair<size_t, size_t> Step() {
+    const size_t from = step_ * grain_;
+    size_t to = (step_ + 1) * grain_ - 1;
+
+    if (to + threshold_ >= num_elements_)
+      to = num_elements_ - 1;
+
+    auto pair = std::make_pair(from, to);
+
+    ++step_;
+
+    return pair;
+  }
+
+private:
+  size_t threshold_;
+  size_t num_elements_;
+  size_t num_threads_;
+  size_t grain_;
+  size_t step_;
+};
 
 int main(int argc, char *argv[]) {
   int opt = 0;
@@ -56,61 +113,52 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  const size_t num_cores = std::thread::hardware_concurrency();
+  std::cout << "generating data..." << std::endl;
 
   // you can use (n(first, last) / 2) for this, but the point of
   // this exercise is mostly to familiarize with concurrency in c++
-  MTVector<std::uint64_t> mt_vector;
-  std::vector<std::uint64_t> vec(max_number);
+  std::vector<std::uint64_t> numbers(max_number);
   {
     std::uint64_t counter = 1;
-    std::generate(vec.begin(),
-                  vec.end(),
+    std::generate(numbers.begin(), numbers.end(),
                   [&counter](){ return counter++; });
   }
 
-  std::cout << "number of items in vector: " << vec.size() << std::endl;
-  std::cout << "number of cores: " << num_cores << std::endl;
+  auto suggestion = Granulator(max_number);
+  std::cout << "number of items in vector: " << numbers.size() << std::endl;
+  std::cout << "number of cores: " << suggestion.GetNumThreads() << std::endl;
+  std::cout << "grain size     : " << suggestion.GetGrain() << std::endl;
 
-  auto fn =
-    [&vec, &mt_vector]
-    (std::vector<std::uint64_t>::iterator begin,
-     std::vector<std::uint64_t>::iterator end) noexcept {
+  MTVector<std::uint64_t> mt_vector;
+  std::vector<std::thread> threads;
+
+  auto work_fn = [](MTVector<std::uint64_t>& mtv,
+                    const std::vector<std::uint64_t>& nms,
+                    const std::pair<size_t,size_t>& rng) noexcept {
     std::uint64_t sum = 0;
-    std::for_each(begin, end, [&sum](const std::uint64_t& val) { sum += val; });
-    mt_vector.Add(sum);
+    for (size_t i = rng.first; i <= rng.second; ++i) sum += nms[i];
+    mtv.Add(sum);
   };
 
-  const int offset = max_number / num_cores;
+  for (size_t i = 0; i < suggestion.GetNumThreads(); ++i) {
+    const auto range = suggestion.Step();
 
-  std::vector<std::thread> threads;
-  threads.reserve(num_cores);
-
-  auto off_a = vec.begin(), off_b = vec.begin();
-  std::advance(off_b, offset);
-
-  for (size_t i = 0; i < num_cores - 1; ++i) {
-    std::cout << "first: " << *off_a << " end:" << *off_b << std::endl;
-    threads.push_back(std::thread(fn, off_a, off_b));
-    std::advance(off_a, offset + 1);
-    std::advance(off_b, offset);
+    threads.push_back(std::thread(work_fn,
+                                  std::ref(mt_vector),
+                                  std::ref(numbers),
+                                  std::ref(range)));
   }
 
-  std::cout << "first: " << *off_a << " end:" << *(vec.end()  - 1)<< std::endl;
-  threads.push_back(std::thread(fn, off_a, vec.end()));
-
-  for (auto it = threads.begin(); it != threads.end(); ++it)
-    (*it).join();
-
-  auto partial_sums = mt_vector.GetUnsafe();
-  for (const auto& e : partial_sums) std::cout << "partial: " << e << std::endl;
+  for (auto& e : threads) e.join();
 
   std::uint64_t final_sum = 0;
-  std::for_each(partial_sums.cbegin(),
-                partial_sums.cend(),
-                [&final_sum](const std::uint64_t v) { final_sum += v; });
+  {
+    auto parts = mt_vector.GetUnsafe();
+    for (const auto& e : mt_vector.GetUnsafe())
+      final_sum += e;
+  }
 
-  std::cout << "final sum: " << final_sum << std::endl;
+  std::cout << "total sum: " << final_sum << std::endl;
 
   return 0;
 }
