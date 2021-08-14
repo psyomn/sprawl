a06a51468818e3eacde3245dd2d0ac4f3a8bb509
Simon Symeonidis <lethaljellybean@gmail.com>
2021-08-13 23:23:48 -0400

    [tools] Add script to generate coverage

---
+++ tools/make-coverage.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+bazel coverage -s \
+  --instrument_test_targets \
+  --experimental_cc_coverage \
+  --combined_report=lcov \
+  --coverage_report_generator=@bazel_tools//tools/test/CoverageOutputGenerator/java/com/google/devtools/coverageoutputgenerator:Main \
+  //...
+
+find ./bazel-out/ -iname "coverage.dat" -print0 | xargs  genhtml -o coverage/
