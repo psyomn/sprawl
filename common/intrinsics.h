83e19bd5ca81e12ef052b5396050db9734ce3ef6
Simon Symeonidis <lethaljellybean@gmail.com>
2021-11-05 00:03:45 -0400

    [common] Move compiler feature stuff elsewhere

---
+++ common/intrinsics.h
@@ -0,0 +1,8 @@
+#ifndef _SPRAWL_COMMON_INTRINSICS_H
+#define _SPRAWL_COMMON_INTRINSICS_H
+
+#ifdef __GNUC__
+# define PURE __attribute__((pure))
+#endif
+
+#endif
--- mdm/intrinsics.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#ifdef __GNUC__
-# define PURE __attribute__((pure))
-#endif // ifdef __GNUC__
